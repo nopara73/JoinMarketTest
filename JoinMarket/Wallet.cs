4ab240d51569833e21c50f6b95f5f1ea13075545
nopara73 <adam.ficsor73@gmail.com>
2015-10-16 00:54:40 +0200

    -

---
+++ JoinMarket/Wallet.cs
@@ -0,0 +1,14 @@
+﻿namespace JoinMarketTest.JoinMarket
+{
+    internal class Wallet
+    {
+        /// <summary>
+        /// Generates a wallet.json without password.
+        /// </summary>
+        /// <returns>JoinMarket console response</returns>
+        internal string Generate()
+        {
+            return "";
+        }
+    }
+}
