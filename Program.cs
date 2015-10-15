b096e78743c07f088bdb55665c0a8e8485f943ee
nopara73 <adam.ficsor73@gmail.com>
2015-10-16 00:50:18 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Add to post build event: 
+ * xcopy "$(ProjectDir)PostBuildEventFiles" "$(TargetDir)" /Y/E/I
+ */
+
+using System;
+using JoinMarketTest.JoinMarket;
+
+namespace JoinMarketTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            var w = new Wallet();
+            Console.Write(w.Generate());
+
+            Console.ReadLine();
+        }
+    }
+}
