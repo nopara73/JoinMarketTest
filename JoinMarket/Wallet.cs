b50e04178d19edeb82b4b959e4f113fc46e16c44
nopara73 <adam.ficsor73@gmail.com>
2015-10-16 02:40:58 +0200

    -

--- JoinMarket/Wallet.cs
+++ JoinMarket/Wallet.cs
@@ -1,14 +1,47 @@
-﻿namespace JoinMarketTest.JoinMarket
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace JoinMarketTest.JoinMarket
 {
     internal class Wallet
     {
+        private readonly string _pythonPath = Path.Combine(Environment.CurrentDirectory, @"Python27/python.exe");
+        private readonly string _walletToolPath = Path.Combine(Environment.CurrentDirectory, @"JoinMarket/wallet-tool.py");
+
         /// <summary>
         /// Generates a wallet.json without password.
         /// </summary>
         /// <returns>JoinMarket console response</returns>
         internal string Generate()
         {
-            return "";
+            return RunPython(_walletToolPath, "generate");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <param name="args"></param>
+        /// <returns>Console response.</returns>
+        private string RunPython(string cmd, string args)
+        {
+            var start = new ProcessStartInfo
+            {
+                FileName = _pythonPath,
+                Arguments = string.Format("{0} {1}", cmd, args),
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            };
+            using (var process = Process.Start(start))
+            {
+                if (process == null) return String.Empty;
+                using (var reader = process.StandardOutput)
+                {
+                    var result = reader.ReadToEnd();
+                    return result;
+                }
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿/*
  * Add to post build event: 
- * xcopy "$(ProjectDir)PostBuildEventFiles\JoinMarket" "$(TargetDir)JoinMarket" /Y/E/I
- * xcopy "$(ProjectDir)PostBuildEventFiles\Python27" "$(TargetDir)Python27" /Y/E/I
- * xcopy "$(ProjectDir)PostBuildEventFiles\libsodium.dll" "$(TargetDir)JoinMarket" /Y/E/I
+ * xcopy "$(ProjectDir)PostBuildEventFiles\JoinMarket" "$(TargetDir)JoinMarket" /Y/E/I/D
+ * xcopy "$(ProjectDir)PostBuildEventFiles\Python27" "$(TargetDir)Python27" /Y/E/I/D
+ * xcopy "$(ProjectDir)PostBuildEventFiles\libsodium.dll" "$(TargetDir)JoinMarket" /Y/E/I/D
  */
 
 using System;
